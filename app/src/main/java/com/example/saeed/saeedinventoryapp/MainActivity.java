71c3a0c69bef025a117f8d03d751d89a4ab4d449
saeed79darvish <s79darvish@gmail.com>
2017-11-02 20:03:17 -0700

    Initial commit

---
+++ app/src/main/java/com/example/saeed/saeedinventoryapp/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.saeed.saeedinventoryapp;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ListView;
+
+import com.example.saeed.saeedinventoryapp.data.InventoryDbHelper;
+import com.example.saeed.saeedinventoryapp.data.StockItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String LOG_TAG = MainActivity.class.getCanonicalName();
+    InventoryDbHelper dbHelper;
+    StockCursorAdapter adapter;
+    int lastVisibleItem = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dbHelper = new InventoryDbHelper(this);
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final ListView listView = (ListView) findViewById(R.id.list_view);
+        View emptyView = findViewById(R.id.empty_view);
+        listView.setEmptyView(emptyView);
+
+        Cursor cursor = dbHelper.readStock();
+
+        adapter = new StockCursorAdapter(this, cursor);
+        listView.setAdapter(adapter);
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                if (scrollState == 0) return;
+                final int currentFirstVisibleItem = view.getFirstVisiblePosition();
+                if (currentFirstVisibleItem > lastVisibleItem) {
+                    fab.show();
+                } else if (currentFirstVisibleItem < lastVisibleItem) {
+                    fab.hide();
+                }
+                lastVisibleItem = currentFirstVisibleItem;
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter.swapCursor(dbHelper.readStock());
+    }
+
+    public void clickOnViewItem(long id) {
+        Intent intent = new Intent(this, DetailsActivity.class);
+        intent.putExtra("itemId", id);
+        startActivity(intent);
+    }
+
+    public void clickOnSale(long id, int quantity) {
+        dbHelper.sellOneItem(id, quantity);
+        adapter.swapCursor(dbHelper.readStock());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_dummy_data:
+                addDummyData();
+                adapter.swapCursor(dbHelper.readStock());
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addDummyData() {
+        StockItem gummibears = new StockItem(
+                "Jack Daniel's Whisky",
+                "17.99 $",
+                10,
+                "Omid Shamsi",
+                "+1 0000000000",
+                "saeed@inventory.com",
+                "android.resource://com.example.saeed.saeedinventoryapp/drawable/jack_daniel_s");
+        dbHelper.insertItem(gummibears);
+
+
+    }
+}
